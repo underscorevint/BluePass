5deaec52889d0f5f909fc59e702d4783553134d9
underscorevint <underscorevint@gmail.com>
2024-01-04 11:57:24 +1300

    Services section update

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () { 
     
- 
+  var partner3images=["./img/partner-31.jpg", "./img/partner-32.jpg", "./img/partner-33.jpg"];
+
       $( '#menu-icon' ).on( "click", function() {
         $( '#navmenu' ).removeClass('hidden');
       });
@@ -14,6 +15,36 @@ $(document).ready(function () {
         $( '#navmenu' ).addClass('hidden');
       });
 
+   
+
+
+
+      setTimeout(looper,2000)
+
+      var partner1images=["./img/partner-11.jpg", "./img/partner-12.jpg", "./img/partner-13.jpg"];
+
+      for(i=0;i<partner1images.length;i++){
+       
+        $('#partners_1').delay(2000).attr("src", partner1images[i]);
+  
+      }
+
+      var partner2images=["./img/partner-21.jpg", "./img/partner-22.jpg", "./img/partner-23.jpg"];
+
+      for(i=0;i<partner2images.length;i++){
+      $('#partners_2').attr("src", partner2images[i]);
+      }
+      
+
+
+      var prt4img=["./img/partner-41.jpg", "./img/partner-42.jpg", "./img/partner-43.jpg"];
+
+      for(i=0;i<prt4img.length;i++){
+        $('#partners_4').attr("src", prt4img[i]);
+      }
+
+
+
  $('.vessel').each(function(i){
     
     var row = $(this);
@@ -22,12 +53,16 @@ $(document).ready(function () {
 
         row.addClass("animated").delay(12000).queue(function(next){
             row.removeClass("animated");
-            next();
+//            next();
         });
 
     }, 12000*i);
 
   });
+
+
+
+
 
  // var i;
  // for (i = 0; i < 5; ++i) {
@@ -66,7 +101,16 @@ $(document).ready(function () {
 //
 }); 
 
+function looper() {
+  
+  var partner3images=["./img/partner-31.jpg", "./img/partner-32.jpg", "./img/partner-33.jpg"];
 
+  for(i=0;i<partner3images.lenght;i++){
+    $('#partners_3').attr("src", partner3images[i]);
+    
+  }
+
+}
 
 //function reveal() {
 //    var reveals = document.querySelectorAll("#blue-pass");
