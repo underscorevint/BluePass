54e35733ab1be1cc48d4d4d9446a0570b8871662
underscorevint <underscorevint@gmail.com>
2024-01-03 16:35:10 +1300

    Additional pages updates

--- build/script.js
+++ build/script.js
@@ -1,3 +1,28 @@
-function SetScrollVar(){
+function BluePassScroll(){
 
-}
+    alert ('!');
+    var parent = document.getElementById('#ship-a');
+
+  };
+
+  object.addEventListener("#bluepass", alert('!'););
+
+// Create the observer like the examples above
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('square-animation');
+        return;
+      }
+  
+      entry.target.classList.remove('square-animation');
+    });
+  });
+  
+  // Get multiple elements instead of a single one using "querySelectorAll"
+  const squares = document.querySelectorAll('#ship-1');
+  
+  // Loop over the elements and add each one to the observer
+  squares.forEach((element) => observer.observe(element));
+
+
