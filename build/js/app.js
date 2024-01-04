99d95568725b234abb4cf0c2d9df28ddb1080a20
underscorevint <underscorevint@gmail.com>
2024-01-04 15:17:14 +1300

    Main Page Changes

--- build/js/app.js
+++ build/js/app.js
@@ -16,49 +16,21 @@ $(document).ready(function () {
       });
 
    
+      $('.login-btn').on( "click", function() {
+        window.open('login.html','_self'); return false;
+      });
+
+      BluePassAnimation();
+
+      PartnersAnimation();
 
 
 
-      setTimeout(looper,2000)
-
-      var partner1images=["./img/partner-11.jpg", "./img/partner-12.jpg", "./img/partner-13.jpg"];
-
-      for(i=0;i<partner1images.length;i++){
-       
-        $('#partners_1').delay(2000).attr("src", partner1images[i]);
-  
-      }
-
-      var partner2images=["./img/partner-21.jpg", "./img/partner-22.jpg", "./img/partner-23.jpg"];
-
-      for(i=0;i<partner2images.length;i++){
-      $('#partners_2').attr("src", partner2images[i]);
-      }
-      
-
-
-      var prt4img=["./img/partner-41.jpg", "./img/partner-42.jpg", "./img/partner-43.jpg"];
-
-      for(i=0;i<prt4img.length;i++){
-        $('#partners_4').attr("src", prt4img[i]);
-      }
 
 
 
- $('.vessel').each(function(i){
-    
-    var row = $(this);
 
-     setTimeout(function() {
 
-        row.addClass("animated").delay(12000).queue(function(next){
-            row.removeClass("animated");
-//            next();
-        });
-
-    }, 12000*i);
-
-  });
 
 
 
@@ -101,14 +73,68 @@ $(document).ready(function () {
 //
 }); 
 
-function looper() {
-  
-  var partner3images=["./img/partner-31.jpg", "./img/partner-32.jpg", "./img/partner-33.jpg"];
+function BluePassAnimation(){
 
-  for(i=0;i<partner3images.lenght;i++){
-    $('#partners_3').attr("src", partner3images[i]);
+  $('.vessel').each(function(i){
     
+    var row = $(this);
+
+     setTimeout(function() {
+
+
+
+        row.toggleClass("animated").delay(8000).queue(function(next){
+            row.toggleClass("animated");
+           next();
+      });
+
+    }, 8000*i);
+
+   // row.delay(8000).removeClass("animated");
+   //setTimeout(BluePassAnimation(),40000);
+
+  });
+
+ 
+}
+
+
+function PartnersAnimation() {
+  
+  var partner1images=["./img/partner-11.jpg", "./img/partner-12.jpg", "./img/partner-13.jpg"];
+
+  for(i=0;i<partner1images.lenght;i++){
+    
+
+    alert(partner1images[i]);
+
+    setTimeout(function() {
+
+      alert('!');
+
+      $('#partners_1').attr("src", partner1images[i]);
+    
+
+  }, 200*i);  
+}
+
+
+
+
+  var partner2images=["./img/partner-21.jpg", "./img/partner-22.jpg", "./img/partner-23.jpg"];
+
+  for(i=0;i<partner2images.length;i++){
+  $('#partners_2').attr("src", partner2images[i]);
   }
+  
+
+
+  var prt4img=["./img/partner-41.jpg", "./img/partner-42.jpg", "./img/partner-43.jpg"];
+
+  for(i=0;i<prt4img.length;i++){
+    $('#partners_4').attr("src", prt4img[i]);
+  }
+
 
 }
 
